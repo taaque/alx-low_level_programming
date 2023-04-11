6bff2499113136bffc166a01d85558583ed5576f
taaque <generalstiffler@gmail.com>
2023-04-10 18:30:47 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,15 +9,15 @@
 	 *
 	 * Return: Always 0 (Success)
 	 */
-int check_num(char *str)
+
+		int check_num(char *str)
 	{
 		/*Declaring variables*/
 		unsigned int count;
 
-
 		count = 0;
-		while (count < strlen(str)) /*count string*/
 
+		while (count < strlen(str)) /*count string*/
 
 		{
 			if (!isdigit(str[count])) /*check if str there are digit*/
@@ -25,12 +25,10 @@ int check_num(char *str)
 				return (0);
 			}
 
-
 			count++;
 		}
 		return (1);
 	}
-
 
 	/**
 	 * main - Print the name of the program
@@ -40,30 +38,24 @@ int check_num(char *str)
 	 * Return: Always 0 (Success)
 	 */
 
-
 	int main(int argc, char *argv[])
 
-
 	{
-
-
 		/*Declaring variables*/
 		int count;
 		int str_to_int;
 		int sum = 0;
 
-
 		count = 1;
+
 		while (count < argc) /*Goes through the whole array*/
 		{
 			if (check_num(argv[count]))
-
 
 			{
 				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 				sum += str_to_int;
 			}
-
 
 			/*Condition if one of the number contains symbols that are not digits*/
 			else
@@ -72,13 +64,10 @@ int check_num(char *str)
 				return (1);
 			}
 
-
 			count++;
 		}
 
-
 		printf("%d\n", sum); /*print sum*/
-
 
 		return (0);
 	}
